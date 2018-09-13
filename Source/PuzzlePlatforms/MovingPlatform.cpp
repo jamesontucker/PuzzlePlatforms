77a44f43d8d56a69c97e63b6188f714df9a852ea
Jameson <jamesoncoletucker@yahoo.com>
2018-09-12 21:02:57 -0400

    Added moving platform with replication

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority())
+	{
+		FVector Location = GetActorLocation();
+		Location += FVector(speed * DeltaTime, 0, 0);
+		SetActorLocation(Location);
+	}
+}
